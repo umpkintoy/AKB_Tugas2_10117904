58273588a33e0182798ffad8bbdf810d1e01cec2
umpkintoy <umpkintoy@users.noreply.github.com>
2020-04-17 18:39:13 +0700

    Create VerifyActivity.java

---
+++ app/src/main/java/id/umpsdev/akbtugas2/VerifyActivity.java
@@ -0,0 +1,14 @@
+package id.umpsdev.akbtugas2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VerifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify);
+    }
+}
