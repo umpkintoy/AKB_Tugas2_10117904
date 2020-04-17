95e688158e5d10f69d39cf69335d12461ed8ecbe
umpkintoy <umpkintoy@users.noreply.github.com>
2020-04-17 18:06:29 +0700

    Update AlmostActivity.java

--- app/src/main/java/id/umpsdev/akbtugas2/AlmostActivity.java
+++ app/src/main/java/id/umpsdev/akbtugas2/AlmostActivity.java
@@ -2,7 +2,9 @@ package id.umpsdev.akbtugas2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AlmostActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class AlmostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_almost);
+    }
+
+    public void btn_Verify (View v){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
