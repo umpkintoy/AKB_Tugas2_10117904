d92ae5e6418af712e83b3913d4ecb032b79003cc
umpkintoy <umpkintoy@users.noreply.github.com>
2020-04-17 16:54:00 +0700

    Update RegisterActivity.java

--- app/src/main/java/id/umpsdev/akbtugas2/RegisterActivity.java
+++ app/src/main/java/id/umpsdev/akbtugas2/RegisterActivity.java
@@ -2,7 +2,9 @@ package id.umpsdev.akbtugas2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void action_back (View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
